4cb9b4530b238f0e276cb69ce12db0ed2ba31c07
andryarys <andryarys@gmail.com>
2015-11-29 15:42:06 +0200

    prepare repo

---
+++ __app/config/db.php
@@ -0,0 +1,8 @@
+<?php defined('_RTMEXEC') or die('No direct script access.');
+
+return array(
+                'hostname'   => 'localhost',
+		'database'   => 'cmswork',
+		'username'   => 'root',
+		'password'   => 'root',
+            );
---
+++ __app/config/smtp.php
@@ -0,0 +1,11 @@
+<?php defined('_RTMEXEC') or die('No direct script access.');
+
+return array(
+                'protocol'    => 'smtp',
+		'host'        => 'ssl://smtp.yandex.ru',
+		'port'        => 465,
+		'user'        => 'andryarys@yandex.ru',
+                'pass'        => 'SagitariaSagitifolia',
+                'project_name'=> 'Community_Title',
+                'method'      => 'smtp'
+            );
