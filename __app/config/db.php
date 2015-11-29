a4fbefa48cfe8623467297a5ceb079acc520be27
andryarys <andryarys@gmail.com>
2015-11-29 16:23:04 +0200

    prepare repo

--- __app/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php defined('_RTMEXEC') or die('No direct script access.');
-
-return array(
-                'hostname'   => 'localhost',
-		'database'   => 'cmswork',
-		'username'   => 'root',
-		'password'   => 'root',
-            );
